aec91ece02a9f20ebbdba82d81e6dfccd11b65f3
zahraard <zahra.ardalani@gmail.com>
2017-06-15 00:43:12 -0500

    admin view added

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,8 @@
 |
 */
 
+use App\User;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,3 +20,9 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('/admin',function(){
+   return view('admin.index');
+});
+
+Route::resource('admin/users','AdminUsersController');
