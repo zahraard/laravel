b63f1ef9fdb4cd13f0e8e799960088d17ea7de70
zahraard <zahra.ardalani@gmail.com>
2017-06-15 22:03:58 -0500

    display user's photos

--- app/Photo.php
+++ app/Photo.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Photo extends Model
 {
     //
+    protected $uploaded='/images/';
     protected $fillable=['file'];
+
+    public function getFileAttribute($photo){
+        return $this->uploaded . $photo;
+    }
+
 }
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -7,6 +7,7 @@
        <thead>
          <tr>
              <th>Id</th>
+             <th>Photo</th>
              <th>Name</th>
              <th>Email</th>
              <th>Role</th>
@@ -20,6 +21,7 @@
        @foreach($users as $user)
          <tr  >
              <td>{{$user->id}}</td>
+             <td><img height="50" src="{{$user->photo ? $user->photo->file : 'No Photo'}}"></td>
              <td>{{$user->name}}</td>
              <td>{{$user->email}}</td>
              <td>{{$user->role->name}}</td>
